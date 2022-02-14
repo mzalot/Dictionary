bd6eda3f5452414d7d65fffee1d02e0745aa545e
mzalot <69329263+mzalot@users.noreply.github.com>
2022-02-14 08:10:58 -0700

    I creaated the repo and initial files

---
+++ src/Dictionary.java
@@ -0,0 +1,11 @@
+/**
+ Dictionary
+ The program will create a Dictionary
+ Author: Mareks Zeile
+ Collaborator(s):
+ Collaboration:
+ Date: 2/14/22
+ On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: MZ
+ **/
+public class Dictionary {
+}
