0204cb8f974c8658039aca8f833891894ff81ed2
mzalot <69329263+mzalot@users.noreply.github.com>
2022-02-14 08:38:00 -0700

    I made all of the instance data, the default constructor, and all the methods except the collection methods.

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -7,5 +7,55 @@
  Date: 2/14/22
  On My Honor, I confirm that I followed all collaboration policy guidelines, and that the work I am submitting is my own: MZ
  **/
-public class Dictionary {
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Dictionary <T, E>{
+    //instance data
+    //arraylists for the key and data
+    private ArrayList<T> values;
+    private ArrayList<E> keys;
+
+    //default constructor
+    public Dictionary(){
+        values = new ArrayList<>();
+        keys = new ArrayList<>();
+    }
+
+    //put method
+    public void put(E key, T value){
+        keys.add(key);
+        values.add(value);
+    }
+
+    //get method
+    public T get(E key){
+        int indexOfKey = keys.indexOf(key);
+        return values.get(indexOfKey);
+    }
+
+    //remove method
+    public T remove(E key){
+        int indexOfKey = keys.indexOf(key);
+        keys.remove(indexOfKey);
+        return values.remove(indexOfKey);
+    }
+
+    //contains method
+    public boolean contains(E key){
+        int indexOfKey = keys.indexOf(key);
+        return indexOfKey >= 0;
+    }
+
+    //isEmpty method
+    public boolean isEmpty(){
+        return values.size() < 0;
+    }
+
+    //size method
+    public int size(){
+        return values.size();
+    }
 }
